d6aa905197f76e8e3718c2400b04841a4dea95a0
siddhi hinge <109226674+siddhihinge25@users.noreply.github.com>
2022-07-14 15:43:10 +0530

    program to find square and qube in c++

---
+++ square cube.cpp
@@ -0,0 +1,21 @@
+//Write programto find out the square and cube of number..
+
+#include<iostream>
+
+#include<cstdlib>
+
+using namespace std;
+
+int main()
+
+{
+
+float n;
+
+cout<<"Enter the number: ";
+
+cin>>n;
+
+cout<<"square of"<<n<<" is "<<n*n<<endl; cout<<"cube of"<<n<<" is "<<n*n*n;
+
+} 
